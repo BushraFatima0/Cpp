10c0173fe07d7b2edf51ca8eb52ef5ea76be8d04
Bushra Fatima <bushrafatima9810@gmail.com>
2025-02-08 16:01:14 +0530

    ques 3.cpp

--- ques 3.cpp
+++ ques 3.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
 using namespace std;
-
-int main() {
+int main() 
+{
     string str, str2;
-    cout << "Enter a string: ";
+    cout << "enter string: ";
     getline(cin, str);
     int l=str.length();
     for (int i = 0; i < l ; i++) 
@@ -32,10 +32,35 @@ int main() {
             break;
         }
     }
+
     if (check==1)
         cout << "It is a palindrome" << endl;
     else
         cout << "It is not a palindrome" << endl;
+    
+    for (int i = 0; i < 26; i++) 
+    {
+        char c = 'a' + i;
+        int count = 0;
+        for (int j=0;j<n;j++) 
+        {
+            if (str2[j] == c) 
+            {
+                count++;
+            }
+        }
+        if (count > 0) 
+        {
+            cout << c << "= " << count << endl;
+        }
+    }
+    for (int i = 0; i < n; i++) 
+    {
+        if (str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u' || str[i] == 'A' || str[i] == 'E' || str[i] == 'I' || str[i] == 'O' || str[i] == 'U') {
+            str[i] = '$';
+        }
+    }
+    cout << "replacing vowels with $: " << str << endl;
 
     return 0;
 }
