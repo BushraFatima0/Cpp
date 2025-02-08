b64d8c3c6007c295c9ade9f100d4b9d132c9b471
Bushra Fatima <bushrafatima9810@gmail.com>
2025-02-08 15:03:59 +0530

    ques 2.cpp

---
+++ ques 2.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    int n;
+    cout << "enter size of array: ";
+    cin >> n;
+    int arr[n], rev[n];
+    int max=arr[0],min=arr[0];
+    int secondmax, secondmin;
+    int i;
+    cout << "enter elements:"<<endl;
+    for (i = 0; i < n; i++) 
+    {
+        cin >> arr[i];
+    }
+    cout << "array: "<<endl;
+    for (i = 0; i < n; i++) 
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+    for (i = 0; i < n; i++) 
+    {
+        rev[i] = arr[n-i-1];
+    }
+    cout << "Reversed array: ";
+    for (i = 0; i < n; i++) 
+    {
+        cout << rev[i] << " ";
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]>max)
+        {
+            secondmax=max;
+            max=arr[i];
+        }
+        else if (arr[i] > secondmax && arr[i] != max) 
+        {
+            secondmax = arr[i];
+        }
+        if(arr[i]<min)
+        {
+            secondmin=min;
+            min=arr[i];
+        }
+        else if (arr[i] < secondmin && arr[i] != min) 
+        {
+            secondmin = arr[i];
+        }
+    }
+    cout << "Second largest element: " << secondmax << endl;
+    cout << "Second smallest element: " << secondmin << endl;
+    return 0;
+
+}
